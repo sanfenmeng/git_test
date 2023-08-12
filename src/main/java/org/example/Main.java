0ba7f245e825c14dcb381ebb6fe28846a8030694
longjiyi <sanfenmeng@foxmail.com>
2023-08-12 23:18:56 +0800

    第三次

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello idea_git");
         System.out.println("hello idea_git");
+        System.out.println("hello idea_git");
 
     }
 }
